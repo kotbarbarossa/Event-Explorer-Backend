53559b259ddffbc7f20f8d6810cba98caedd0992
Kot Barba <kot.barbarossa@yandex.com>
2023-10-24 22:39:19 +0300

    Event and Place model create. User subscriptions, place user association and event participants.

--- backend/models.py
+++ backend/models.py
@@ -1,16 +1,16 @@
 from sqlalchemy import (Column, Integer, String,
                         MetaData, DateTime, Boolean,
-                        # ForeignKey,
-                        Enum)
+                        Enum, Text, ForeignKey, Table)
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql import func
-# from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship
 
 metadata = MetaData()
 Base = declarative_base(metadata=metadata)
 
 
 class Command(Base):
+    """Модель команды."""
     __tablename__ = 'commands'
 
     id = Column(Integer, primary_key=True, index=True)
@@ -23,6 +23,7 @@ class Command(Base):
 
 
 class Message(Base):
+    """Модель сообщения."""
     __tablename__ = 'messages'
 
     id = Column(Integer, primary_key=True, index=True)
@@ -34,7 +35,55 @@ class Message(Base):
         self.response = response
 
 
+# Таблицы для связей между пользователями, местами и событиями
+user_subscriptions = Table(
+    'user_subscriptions',
+    Base.metadata,
+    Column(
+        'subscriber_id',
+        String,
+        ForeignKey('users.telegram_id'),
+        primary_key=True),
+    Column(
+        'user_id',
+        String,
+        ForeignKey('users.telegram_id'),
+        primary_key=True),
+)
+
+place_user_association = Table(
+    'place_user_association',
+    Base.metadata,
+    Column(
+        'place_id',
+        String,
+        ForeignKey('places.place_id'),
+        primary_key=True),
+    Column(
+        'user_id',
+        String,
+        ForeignKey('users.telegram_id'),
+        primary_key=True),
+)
+
+event_participants = Table(
+    'event_participants',
+    Base.metadata,
+    Column(
+        'event_id',
+        Integer,
+        ForeignKey('events.id'),
+        primary_key=True),
+    Column(
+        'user_id',
+        String,
+        ForeignKey('users.telegram_id'),
+        primary_key=True),
+)
+
+
 class User(Base):
+    """Модель пользователя."""
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, index=True)
@@ -53,3 +102,82 @@ class User(Base):
     created_date = Column(DateTime(timezone=True), server_default=func.now())
     modified_date = Column(DateTime(timezone=True), onupdate=func.now())
     comment = Column(String, default='', nullable=True)
+
+    # Многие ко многим: пользователь подписывается на других пользователей
+    subscriptions = relationship(
+        'User',
+        secondary=user_subscriptions,
+        primaryjoin=id == user_subscriptions.c.subscriber_id,
+        secondaryjoin=id == user_subscriptions.c.user_id,
+        back_populates='subscribers',
+    )
+
+    subscribers = relationship(
+        'User',
+        secondary=user_subscriptions,
+        primaryjoin=id == user_subscriptions.c.user_id,
+        secondaryjoin=id == user_subscriptions.c.subscriber_id,
+        back_populates='subscriptions',
+    )
+
+
+class Place(Base):
+    """Модель места."""
+    __tablename__ = 'places'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True, nullable=True)
+    place_id = Column(String, unique=True, nullable=False, index=True)
+    events = relationship('Event', back_populates='place')
+
+    subscribers = relationship(
+        'User',
+        secondary=place_user_association,
+        primaryjoin=id == place_user_association.c.place_id,
+        secondaryjoin=User.id == place_user_association.c.user_id,
+        back_populates='favorite_places',
+    )
+
+
+class Event(Base):
+    """Модель события."""
+    __tablename__ = 'events'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, default='Событие')
+    description = Column(Text, nullable=True)
+    user_id = Column(String, ForeignKey('users.telegram_id'), nullable=False)
+    place_id = Column(String, ForeignKey('places.place_id'), nullable=False)
+    start_datetime = Column(DateTime, nullable=False)
+    end_datetime = Column(DateTime, nullable=False)
+    comment = Column(String, nullable=True)
+
+    place = relationship('Place', back_populates='events')
+
+    participants = relationship(
+        'User',
+        secondary=event_participants,
+        primaryjoin=id == event_participants.c.event_id,
+        secondaryjoin=User.id == event_participants.c.user_id,
+        back_populates='events_participated',
+    )
+
+
+# Многие ко многим: пользователь подписывается на места
+User.favorite_places = relationship(
+    'Place',
+    secondary=place_user_association,
+    primaryjoin=User.id == place_user_association.c.user_id,
+    secondaryjoin=Place.id == place_user_association.c.place_id,
+    back_populates='subscribers',
+)
+
+
+# Многие ко многим: пользователь участвует в событиях
+User.events_participated = relationship(
+    'Event',
+    secondary=event_participants,
+    primaryjoin=User.id == event_participants.c.user_id,
+    secondaryjoin=Event.id == event_participants.c.event_id,
+    back_populates='participants',
+)
